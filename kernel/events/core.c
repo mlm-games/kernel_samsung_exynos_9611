9c964d705bc612a7d0c0c835ef11f1e35e8d8cbc
Tim Zimmermann <tim@linux4.de>
2023-02-16 20:25:46 +0100

    Import miscellaneous P610XXU2FWA5 changes
    Change-Id: I16e6bedad2b26a591c32f918383c174e1d90c6d7

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -10116,6 +10116,11 @@ SYSCALL_DEFINE5(perf_event_open,
 	if (err)
 		return err;
 
+	/* Do we allow access to perf_event_open(2) ? */
+	err = security_perf_event_open(&attr, PERF_SECURITY_OPEN);
+	if (err)
+		return err;
+
 	err = perf_copy_attr(attr_uptr, &attr);
 	if (err)
 		return err;
