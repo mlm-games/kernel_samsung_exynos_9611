972b2425d66d30a905a6f5bba622e7a270d2c5e5
Tim Zimmermann <tim@linux4.de>
2024-04-02 20:04:30 +0200

    input: Sync switch event codes with upstream linux
    * They are checked as of https://android.googlesource.com/platform/frameworks/native/+/9762ac9c1b813850379f55dfc2bd6698b8c96c64,
      values > upstream linux SW_MAX will now cause a system_server crash
    
    Change-Id: Ib26f230bfa663f78ec17fc91890679210558ad60

--- drivers/input/misc/hall/hall_ic_wacom.c
+++ drivers/input/misc/hall/hall_ic_wacom.c
@@ -284,7 +284,7 @@ static int wacom_hall_probe(struct platform_device *pdev)
 
 	input->evbit[0] |= BIT_MASK(EV_SW);
 
-	ddata->event_val = SW_WACOM_HALL;
+	ddata->event_val = SW_MACHINE_COVER;
 
 	input_set_capability(input, EV_SW, ddata->event_val);
 
