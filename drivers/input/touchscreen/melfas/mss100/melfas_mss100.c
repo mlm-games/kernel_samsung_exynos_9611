Roynas-Android-Playground/kernel_samsung_universal9611

melfas/mss100: limit physical proximity sense range
* On stock ROM proximity only changes if changes occur near to the sensor. However on AOSP proximity already changes if a finger is placed somewhere between the upper half and the middle of the screen, making it impossible to for example pull down the notification panel during a phone call because the screen constantly turnes off. Samsung seems to apply something remotely similar on their stock ROM by creating a whole new virtual sensor.
* We now check if the area around the sensor (1400-2800 on the X axis, 0-700 on the Y axis) is touched and only then report proximity changes.
* To not break proximity reporting during screen off we also check if the panel is in LPM state

Change-Id: I22cb2f15cbb8a852b416642b47e1eb2799053d7e

drivers/input/touchscreen/melfas/mss100/melfas_mss100.c
@@ -815,7 +815,9 @@ static int mms_alert_handler_pocket_mode_state(struct mms_ts_info *info, u8 data
 
 static int mms_alert_handler_proximity_state(struct mms_ts_info *info, u8 data)
 {
-	
+#ifndef GRASS_ONEUI
+	bool report = false;
+#endif
 	if (!info->dtdata->support_protos) {
 		if (data == 4 || data == 5) {
 			input_info(true, &info->client->dev, "%s: not support protos %d\n", __func__, data);
@@ -823,7 +825,23 @@ static int mms_alert_handler_proximity_state(struct mms_ts_info *info, u8 data)
 		}
 	}
 #ifndef GRASS_ONEUI
-	data = data == 5 || !data;
+	report |= info->lowpower_mode;
+	if (info->touch_count > 0) {
+		int i;
+		for (i = 0; i < MAX_FINGER_NUM; i++) {
+			if (info->finger_state[i]) {
+				if (info->coord[i].y < 700 && info->coord[i].x > 900 && info->coord[i].x < 3000) {
+					report |= true;
+					break;
+				}
+			}
+		}
+	}
+	if (report) {
+		data = data == 5 || !data;
+	} else {
+		data = 1;
+	}
 #endif
 	input_info(true, &info->client->dev, "%s: hover %d\n", __func__, data);
 	info->hover_event = data;

mlm-games/kernel_samsung_exynos_9611

drivers/input/touchscreen/melfas/mss100/melfas_mss100.c
@@ -815,14 +815,34 @@ static int mms_alert_handler_pocket_mode_state(struct mms_ts_info *info, u8 data
 
 static int mms_alert_handler_proximity_state(struct mms_ts_info *info, u8 data)
 {
-	
+#ifndef CONFIG_ONEUI
+	bool report = false;
+#endif
 	if (!info->dtdata->support_protos) {
 		if (data == 4 || data == 5) {
 			input_info(true, &info->client->dev, "%s: not support protos %d\n", __func__, data);
 			return 0;
 		}
 	}
-	data = data == 5 || !data;
+#ifndef CONFIG_ONEUI
+	report |= info->lowpower_mode;
+	if (info->touch_count > 0) {
+		int i;
+		for (i = 0; i < MAX_FINGER_NUM; i++) {
+			if (info->finger_state[i]) {
+				if (info->coord[i].y < 700 && info->coord[i].x > 900 && info->coord[i].x < 3000) {
+					report |= true;
+					break;
+				}
+			}
+		}
+	}
+	if (report) {
+		data = data == 5 || !data;
+	} else {
+		data = 1;
+	}
+#endif
 	input_info(true, &info->client->dev, "%s: hover %d\n", __func__, data);
 	info->hover_event = data;
 